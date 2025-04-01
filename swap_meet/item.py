84313bda3c5d31a41a757040987b6ce30cd14686
collette7 <cece554@gmail.com>
2025-04-01 08:31:15 -0500

    1. add case for generating random id's 2. add case for and and removing inventory and searching by id

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
+import random
+
 class Item:
-    pass
+    # generate random id's
+    def __init__(self, id=None):
+        if id is None:
+            random_num = ""
+            for i in range(32):
+                random_num += str(random.randint(0, 9))
+            self.id = int(random_num)
+        else:
+            self.id = id
+    
+    def get_category(self):
+        return "Item"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,31 @@
 class Vendor:
-    ### Wave 1
+    def __init__(self, inventory = None):
+        if inventory == None:
+            inventory = []
+        self.inventory = inventory
+        
+    # Adds an item to the inventory
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    # Removes an item from the vendors inventory
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        if item in self.inventory:
+            self.inventory.remove(item)
+        return item
+    
+    # Searche through the vendor's inventory for an item with a matching id
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
 
+
+### Wave 1
 # In Wave 1 we will create the `Vendor` class.
 # - There is a module (file) named `vendor.py` inside of the `swap_meet` package (folder)
 # - Inside this module, there is a class named `Vendor`
@@ -13,22 +38,3 @@ class Vendor:
 # - This method removes the matching item from the `inventory`
 # - This method returns the item that was removed
 # - If there is no matching item in the `inventory`, the method should return `False`
-
-    def __init__(self, inventory = None):
-        if inventory == None:
-            inventory = []
-        self.inventory = inventory
-        
-    def add(self, item):
-        self.inventory.append(item)
-        return item
-    
-    def remove(self, item):
-        if item not in self.inventory:
-            return False
-        if item in self.inventory:
-            self.inventory.remove(item)
-        return item
-        
-
-
