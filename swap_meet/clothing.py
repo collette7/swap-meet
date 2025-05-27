2d9f10f441b097c57c4af9796e5dde2d65b1875d
Flora <141187313+FloritaZhao@users.noreply.github.com>
2025-05-26 22:03:44 -0700

    Update swap_meet/clothing.py
    Co-authored-by: apradoada <110429145+apradoada@users.noreply.github.com>

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -6,4 +6,4 @@ class Clothing(Item):
         self.fabric = fabric
 
     def __str__(self):
-        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+        return f"{super().__str__()} It is made from {self.fabric} fabric."
